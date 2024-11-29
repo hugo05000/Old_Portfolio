8d45170c42a329f6c4df5bc587f1f79fa472ea8b
hugomarceau <marceauhugo@gmail.com>
2024-11-29 14:38:10 +0100

    pour publication sur le WEB

--- js/app.js
+++ js/app.js
@@ -30,6 +30,31 @@ $(document).ready(function() {
     });
 });
 
+
+// Fonction pour vérifier si l'élément est visible dans la fenêtre
+function isInViewport(element) {
+    const rect = element.getBoundingClientRect();
+    return rect.top <= window.innerHeight && rect.bottom >= 0;
+}
+
+const boxes = document.querySelectorAll('.box');
+
+window.addEventListener('scroll', () => {
+    boxes.forEach((box) => {
+        if (isInViewport(box)) {
+            box.classList.add('visible');
+        }
+    });
+});
+
+// Exemple de basculement horizontal ou vertical
+document.querySelectorAll('.box').forEach((box) => {
+    // Ajoutez la classe "vertical" pour une animation verticale
+    if (box.dataset.animation === "vertical") {
+        box.classList.add('vertical');
+    }
+});
+
 //Modals
 
 const modalContainerAgra = document.querySelector(".modal-container-agra");
@@ -56,47 +81,3 @@ function toggleModalMa(){
 function toggleModalCv(){
     modalContainerCv.classList.toggle("active")
 }
-
-
-//Barres de progression -> Profil
-
-let number1 = document.getElementById("number1");
-let number2 = document.getElementById("number2");
-let number3 = document.getElementById("number3");
-let number4 = document.getElementById("number4");
-
-let counter1 = 0;
-let counter2 = 0;
-let counter3 = 0;
-let counter4 = 0;
-
-setInterval(() => {
-    if(counter1 == 90){
-        clearInterval();
-    }else{
-        counter1 += 1;
-        number1.innerHTML = counter1 + "%";
-    }
-
-    if(counter2 == 65){
-        clearInterval();
-    }else{
-        counter2 += 1;
-        number2.innerHTML = counter2 + "%";
-    }
-
-    if(counter3 == 80){
-        clearInterval();
-    }else{
-        counter3 += 1;
-        number3.innerHTML = counter3 + "%";
-    }
-
-    if(counter4 == 65){
-        clearInterval();
-    }else{
-        counter4 += 1;
-        number4.innerHTML = counter4 + "%";
-    }
-},20);
-
